7d56cca4b62421f4861c53c817db870d4911b868
Hadixp <hadi.b.2002@gmail.com>
2025-10-04 02:30:10 +0330

    db tests

--- index.js
+++ index.js
@@ -2,20 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const config = require('config')
+const _ = require('lodash')
 mongoose.connect('mongodb://localhost:27017/elhamStyle')
 .then(()=>{console.log('connected to data base');})
 .catch(()=>{console.log('not connected');})
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors()); // enable CORS for all routes
 const BodySize = require('./schema/userBodySize');
 
-app.get('/testAPI',(req,res)=>{
-    res.json({
-        data:'this is a test msg'
-    })
-});
+app.post('/getSize',async (req,res)=>{
+    let userSize = new BodySize(
+        _.pick(req.body,['Name','Email','Bust','Waist','Hip','HighHip','bodyShape'])
+    )
+    userSize = await userSize.save();
+    res.json({data:userSize,msg:'your size saved'})
 
+})
 
 
 
