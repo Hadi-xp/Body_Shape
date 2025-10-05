1a65869b998446ba194aa06307570af6488d985b
Hadixp <hadi.b.2002@gmail.com>
2025-10-05 22:28:08 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const config = require('config')
-const _ = require('lodash')
+const config = require('config');
+const _ = require('lodash');
 mongoose.connect('mongodb://localhost:27017/elhamStyle')
 .then(()=>{console.log('connected to data base');})
 .catch(()=>{console.log('not connected');})
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors()); // enable CORS for all routes
+app.use(cors());
 const BodySize = require('./schema/userBodySize');
 
 app.post('/getSize',async (req,res)=>{
